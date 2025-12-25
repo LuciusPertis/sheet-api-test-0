3a9f4d876625e02c116cb1469e780be7d41e8a21
--help <luciuspertis@gmail.com>
2025-12-26 02:48:49 +0530

    upd: deploy ME Anyone

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const AppConfig = {
     // The URL you got from deploying the Google Script
-    scriptUrl: "https://script.google.com/a/macros/iiita.ac.in/s/AKfycbwqZp_A95JrU0lyQmzZCG4nZUKAs4EA86uVDAwyh6uqT9vRkepl5OzC5CURtTSXhMvI/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbwqZp_A95JrU0lyQmzZCG4nZUKAs4EA86uVDAwyh6uqT9vRkepl5OzC5CURtTSXhMvI/exec",
     
     // Optional: Define expected table headers for your UI to generate
     headers: ["Timestamp", "Name", "Role"]
